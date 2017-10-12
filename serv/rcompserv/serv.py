74d668cb8291822a167d1ddd0fecf7e580375377
Scott C. Livingston <slivingston@cds.caltech.edu>
2017-10-11 18:11:14 -0700

    Add route for `trivial` (vacuous) command

--- serv/rcompserv/serv.py
+++ serv/rcompserv/serv.py
@@ -1,4 +1,8 @@
+import uuid
+from datetime import datetime
+
 from aiohttp import web
+import redis
 
 from . import __version__
 
@@ -8,15 +12,35 @@ class Server:
         self._host = host
         self._port = port
         self.app = web.Application()
+        self.app.on_startup.append(self.start_redis)
         self.app.router.add_get('/', self.index)
-        self.known_commands = ['version']
+        self.known_commands = ['version', 'trivial']
         self.app.router.add_get('/version', self.version)
+        self.app.router.add_get('/trivial', self.trivial)
+
+    async def start_redis(self, app):
+        app['redis'] = redis.StrictRedis()
 
     async def index(self, request):
         return web.json_response({'commands': self.known_commands})
 
     async def version(self, request):
         return web.json_response({'version': __version__})
+
+    async def trivial(self, request):
+        job_id = str(uuid.uuid4())
+        start_time = str(datetime.utcnow())
+        request.app['redis'].hset(job_id, 'cmd', 'trivial')
+        request.app['redis'].hset(job_id, 'stime', start_time)
+        request.app['redis'].hset(job_id, 'done', 1)
+        request.app['redis'].hset(job_id, 'output', '')
+        return web.json_response({
+            'cmd': str(request.app['redis'].hget(job_id, 'cmd'), encoding='utf-8'),
+            'id': job_id,
+            'stime': str(request.app['redis'].hget(job_id, 'stime'), encoding='utf-8'),
+            'done': False if request.app['redis'].hget(job_id, 'done') == 0 else True,
+            'output': str(request.app['redis'].hget(job_id, 'output'), encoding='utf-8')
+        })
 
     def run(self):
         web.run_app(self.app, host=self._host, port=self._port)
