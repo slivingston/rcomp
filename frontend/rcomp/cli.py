85cf3ed3a571277a988599ba98304bc889186e0f
Scott C. Livingston <slivingston@cds.caltech.edu>
2017-10-14 16:46:10 -0700

    prefix rcomp CLI switches with `--rcomp-`
    This is motivated to avoid collisions or confusion with command-line
    switches intended for programs that are run via `rcomp`.

--- frontend/rcomp/cli.py
+++ frontend/rcomp/cli.py
@@ -12,15 +12,21 @@ from . import __version__
 
 
 def main(argv=None):
-    parser = argparse.ArgumentParser(prog='rcomp')
-    parser.add_argument('-V', '--version', action='store_true', dest='show_version',
+    parser = argparse.ArgumentParser(prog='rcomp', add_help=False)
+    parser.add_argument('--rcomp-help', action='store_true', dest='show_help',
+                        help='print this help message and exit')
+    parser.add_argument('--rcomp-version', action='store_true', dest='show_version',
                         help='print version number and exit')
-    parser.add_argument('-s', '--server', metavar='URI', dest='basi_uri',
+    parser.add_argument('--rcomp-server', metavar='URI', dest='basi_uri',
                         help='base URI for job requests. (default is https://api.fmtools.org)')
     if argv is None:
         args = parser.parse_args()
     else:
         args = parser.parse_args(argv)
+
+    if args.show_help:
+        parser.print_help()
+        return 0
 
     if args.show_version:
         print('rcomp '+__version__)
