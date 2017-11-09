4b6b187bfdcaec906943e8fa73907513c053cec1
Scott C. Livingston <slivingston@cds.caltech.edu>
2017-11-08 19:03:00 -0800

    consolidate HTTP request formation in JavaScript client

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,10 +2,40 @@ const https = require('https');
 const http = require('http');
 const url = require('url');
 
-const default_host = {
-    hostname: 'api.fmtools.org',
-    port: 443
-};
+
+function createProtoRequest( options, base_uri ) {
+    var options = options;
+    if (options === undefined) {
+        options = {};
+    } else if ((typeof options === 'string') && base_uri === undefined) {
+        var base_uri = options;
+        options = {};
+    }
+    if (base_uri) {
+        var new_host = url.parse(base_uri);
+        options.hostname = new_host.hostname;
+        options.port = new_host.port;
+        if (new_host.protocol === 'http:') {
+            if (options.port == null) {
+                options.port = '80';
+            }
+            var scheme = http;
+        } else {  // new_host.protocol === 'https:'
+            if (options.port == null) {
+                options.port = '443';
+            }
+            var scheme = https;
+        }
+    } else {
+        options.hostname = 'api.fmtools.org';
+        options.port = 443;
+        var scheme = https;
+    }
+
+    return (function (f) {
+        return scheme.request(options, f);
+    });
+}
 
 
 // getIndex( result_function, base_uri )
@@ -19,36 +49,20 @@ const default_host = {
 // to which the request should be sent. The default value is
 // equivalent to https://api.fmtools.org
 exports.getIndex = (function (result_function, base_uri) {
-    if (base_uri) {
-        var new_host = url.parse(base_uri);
-        var options = {
-            hostname: new_host.hostname,
-            port: new_host.port
-        };
-        if (new_host.protocol === 'http:') {
-            if (options.port == null) {
-                options.port = '80';
-            }
-            var scheme = http;
-        } else {  // new_host.protocol === 'https:'
-            if (options.port == null) {
-                options.port = '443';
-            }
-            var scheme = https;
-        }
-    } else {
-        var options = default_host;
-        var scheme = https;
-    }
-    var req = scheme.request(options, (res) => {
-        res.on('data', (data) => {
+    var reqf = createProtoRequest( base_uri );
+    var data = '';
+    var req = reqf((res) => {
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
             result_function(JSON.parse(data));
         });
     });
-    req.end();
     req.on('error', (err) => {
         console.error(err);
     });
+    req.end();
 });
 
 // getServerVersion( result_function, base_uri )
@@ -60,35 +74,18 @@ exports.getIndex = (function (result_function, base_uri) {
 // to which the request should be sent. The default value is
 // equivalent to https://api.fmtools.org
 exports.getServerVersion = (function (result_function, base_uri) {
-    if (base_uri) {
-        var new_host = url.parse(base_uri);
-        var options = {
-            hostname: new_host.hostname,
-            port: new_host.port
-        };
-        if (new_host.protocol === 'http:') {
-            if (options.port == null) {
-                options.port = '80';
-            }
-            var scheme = http;
-        } else {  // new_host.protocol === 'https:'
-            if (options.port == null) {
-                options.port = '443';
-            }
-            var scheme = https;
-        }
-    } else {
-        var options = default_host;
-        var scheme = https;
-    }
-    options.path = '/version';
-    var req = scheme.request(options, (res) => {
-        res.on('data', (data) => {
+    var reqf = createProtoRequest( {path: '/version'}, base_uri );
+    var data = '';
+    var req = reqf((res) => {
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
             result_function(JSON.parse(data));
         });
     });
-    req.end();
     req.on('error', (err) => {
         console.error(err);
     });
+    req.end();
 });
